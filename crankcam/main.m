c7bf36a42beec72ac7a83c432627617a9348b33b
Mitchell Hart <mitchellhart@Mitchells-MacBook-Air.local>
2014-07-31 00:36:40 -0400

    Initial Commit

---
+++ crankcam/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  crankcam
+//
+//  Created by Mitchell Hart on 7/31/14.
+//  Copyright (c) 2014 Superfort. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SFAppDelegate class]));
+    }
+}
---
+++ crankcamTests/crankcamTests.m
@@ -0,0 +1,34 @@
+//
+//  crankcamTests.m
+//  crankcamTests
+//
+//  Created by Mitchell Hart on 7/31/14.
+//  Copyright (c) 2014 Superfort. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface crankcamTests : XCTestCase
+
+@end
+
+@implementation crankcamTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
